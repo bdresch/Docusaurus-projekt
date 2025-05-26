05d1f4114b721c6f5e833b534e8247e3aefd8693
Borbala Drescher <borbala.drescher@ixenit.com>
2025-05-26 13:49:11 +0200

    Javítottam a docusaurus config fájlt

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://bdresch.githu.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'https://github.com/bdresch/Docusaurus-projekt',
+  baseUrl: '/Docusaurus-projekt/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

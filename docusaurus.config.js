0171ecb2277f2ab89116d6281f08d8f119c09b6a
Borbala Drescher <borbala.drescher@ixenit.com>
2025-05-26 13:31:33 +0200

    Fix: helyes baseUrl GitHub Pages-re

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://bdresch.githu.io',
+  url: 'https://bdresch.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'https://github.com/bdresch/Docusaurus-projekt',
+  baseUrl: '/Docusaurus-projekt/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
